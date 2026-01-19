36f167be38f6034ef73163f021706ad4beefe578
Gabriel Felix <gabrielfelix099@hotmail.com>
2026-01-19 20:05:19 -0300

    feat/free-code-camp: Resolução do desafio de construir um programa que gere uma média GPA

--- Program.cs
+++ Program.cs
@@ -4,229 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\n=== Exercício 01 ===\n");
+        CalculadoraGPA();
+    }
 
-        List<int> listaDeNumerosDecrescentes = new List<int>();
+    static void CalculadoraGPA()
+    {
+        string nomeAluno = "Sophia Johnson";
+        string cursoEscola1 = "Inglês 101";
+        string cursoEscola2 = "Algebra 101 ";
+        string cursoEscola3 = "Biologia 101";
+        string cursoEscola4 = "Ciência da computação I";
+        string cursoEscola5 = "Psicologia 101";
 
-        Console.WriteLine("Números crescentes:");
-        for (int numerosCrescentes = 1; numerosCrescentes <= 10; numerosCrescentes++)
-        {
-            Console.WriteLine(numerosCrescentes);
-            listaDeNumerosDecrescentes.Add(numerosCrescentes);
-        }
+        int horasCreditoCurso1 = 3;
+        int horasCreditoCurso2 = 3;
+        int horasCreditoCurso3 = 4;
+        int horasCreditoCurso4 = 4;
+        int horasCreditoCurso5 = 3;
 
-        listaDeNumerosDecrescentes.Reverse();
+        int grauA = 4;
+        int grauB = 3;
 
-        Console.WriteLine("\nNúmeros decrescentes:");
+        int curso1Grau = grauA;
+        int curso2Grau = grauB;
+        int curso3Grau = grauB;
+        int curso4Grau = grauB;
+        int curso5Grau = grauA;
 
-        foreach (var itensNaListaDecrescente in listaDeNumerosDecrescentes)
-            Console.WriteLine(itensNaListaDecrescente);
+        int horasCreditoTotais = 0;
+        horasCreditoTotais += horasCreditoCurso1;
+        horasCreditoTotais += horasCreditoCurso2;
+        horasCreditoTotais += horasCreditoCurso3;
+        horasCreditoTotais += horasCreditoCurso4;
+        horasCreditoTotais += horasCreditoCurso5;
 
-        Console.WriteLine("\n=== Exercício 02 ===\n");
+        int pontosGrauTotais = 0;
+        pontosGrauTotais += horasCreditoCurso1 * curso1Grau;
+        pontosGrauTotais += horasCreditoCurso2 * curso2Grau;
+        pontosGrauTotais += horasCreditoCurso3 * curso3Grau;
+        pontosGrauTotais += horasCreditoCurso4 * curso4Grau;
+        pontosGrauTotais += horasCreditoCurso5 * curso5Grau;
 
-        while (true)
-        {
-            Console.WriteLine($"Informe um número para a tabuada:");
-            string numeroDigitado = Console.ReadLine();
+        decimal grauDePontosMaximos = (decimal)pontosGrauTotais / horasCreditoTotais;
 
-            decimal numeroTabuada, multiplicacaoTabuada;
+        int digitoPrincipal = (int)grauDePontosMaximos;
+        int primeiroDigito = (int)(grauDePontosMaximos * 10) % 10;
+        int segundoDigito = (int)(grauDePontosMaximos * 100) % 10;
 
-            if (decimal.TryParse(numeroDigitado, out numeroTabuada))
-            {
-                Console.WriteLine($"\nTabuada do número: {numeroTabuada}");
+        Console.WriteLine($"Estudande: {nomeAluno}\n");
+        Console.WriteLine($"Curso\t\t\tGrau\tCrédito Horas");
+        Console.WriteLine($"{cursoEscola1}\t\t{curso1Grau}\t\t{horasCreditoCurso1}");
+        Console.WriteLine($"{cursoEscola2}\t\t{curso2Grau}\t\t{horasCreditoCurso2}");
+        Console.WriteLine($"{cursoEscola3}\t\t{curso3Grau}\t\t{horasCreditoCurso3}");
+        Console.WriteLine($"{cursoEscola4}\t{curso4Grau}\t\t{horasCreditoCurso4}");
+        Console.WriteLine($"{cursoEscola5}\t\t{curso5Grau}\t\t{horasCreditoCurso5}");
 
-                for (decimal numeroParaMultiplicar = 1; numeroParaMultiplicar <= 10; numeroParaMultiplicar++)
-                {
-                    multiplicacaoTabuada = numeroTabuada * numeroParaMultiplicar;
-                    Console.WriteLine($"{numeroTabuada} X {numeroParaMultiplicar} = {multiplicacaoTabuada}");
-                }
-                break;
-            }
-            else
-                Console.WriteLine($"Valor digitado inválido\n");
-        }
-
-        Console.WriteLine("\n=== Exercício 03 ===\n");
-
-        while (true)
-        {
-            Console.WriteLine("Digite a temperatura em °C para ser convertida: ");
-            string numeroInformadoParaConversao = Console.ReadLine();
-
-            decimal temperaturaEmCelcius, conversaoEmFahrenheit;
-
-            if (decimal.TryParse(numeroInformadoParaConversao, out temperaturaEmCelcius))
-            {
-                conversaoEmFahrenheit = (temperaturaEmCelcius * 9 / 5) + 32;
-
-                Console.WriteLine($"\nTemperatura em °C informada: {temperaturaEmCelcius}°");
-                Console.WriteLine($"Temperatura em Fahrenheit: {conversaoEmFahrenheit}°");
-                break;
-            }
-            else
-                Console.WriteLine("Valor informado inválido");
-        }
-
-        Console.WriteLine("\n=== Exercício 04 ===\n");
-
-        int numerosDigitados;
-        int somaDeNumeros = 0;
-
-        while (true)
-        {
-            Console.WriteLine("Digite números positivos para soma: ");
-            numerosDigitados = int.Parse(Console.ReadLine());
-
-            if (numerosDigitados < 0)
-                break;
-
-            somaDeNumeros += numerosDigitados;
-        }
-
-        Console.WriteLine($"Soma total dos números: {somaDeNumeros}");
-
-        Console.WriteLine("\n=== Exercício 05 ===\n");
-
-        Console.WriteLine("Digite um número: ");
-        int numeroDigitado3 = int.Parse(Console.ReadLine());
-
-        List<int> listaDeNumerosPrimos = new List<int>();
-
-        for (int contadorNumeroPrimo = 2; contadorNumeroPrimo <= numeroDigitado3; contadorNumeroPrimo++)
-        {
-            bool numeroPrimo = true;
-
-            for (int multiplicadorPrimo = 2; multiplicadorPrimo * multiplicadorPrimo <= contadorNumeroPrimo; multiplicadorPrimo++)
-            {
-                if (contadorNumeroPrimo % multiplicadorPrimo == 0)
-                {
-                    numeroPrimo = false;
-                    continue;
-                }
-            }
-
-            if (numeroPrimo)
-            {
-                listaDeNumerosPrimos.Add(contadorNumeroPrimo);
-                continue;
-            }
-        }
-
-        Console.WriteLine($"Números primos existentes até o número {numeroDigitado3}: ");
-        foreach (var itemListaNumerosPrimos in listaDeNumerosPrimos)
-        {
-            Console.Write($"{itemListaNumerosPrimos} ");
-        }
-
-        Console.WriteLine("\n=== Exercício 06 ===\n");
-
-        Console.WriteLine("Digite uma palavra ou um frase para contarmos quantas vogais há: ");
-        string informacaoDigitada1 = Console.ReadLine();
-
-        int contadorDeCaracteres = 0;
-
-        char[] listaDeCaracteres1 = informacaoDigitada1.ToCharArray();
-
-        for (int percorreLista = 0; percorreLista < listaDeCaracteres1.Length; percorreLista++)
-        {
-            switch (listaDeCaracteres1[percorreLista])
-            {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                    contadorDeCaracteres++;
-                    break;
-                default:
-                    continue;
-            }
-        }
-
-        Console.WriteLine($"\nSeu texto digitado: {informacaoDigitada1}");
-        Console.WriteLine($"Quantidade de vogais presentes: {contadorDeCaracteres}");
-
-
-        Console.WriteLine("\n=== Exercício 07 ===\n");
-
-        Console.WriteLine("Digite uma palavra ou uma frase: ");
-        string informacaoDigitada = Console.ReadLine();
-
-        //string textoSemEspaco = informacaoDigitada.Replace(" ", "");
-
-        Console.WriteLine($"\nSeu texto: {informacaoDigitada}");
-
-        char[] listaDeCaracteres = informacaoDigitada.ToArray();
-
-        Array.Reverse(listaDeCaracteres);
-
-        Console.Write("\nSeu texto invertido: ");
-        foreach (var itensListaDeCaracteres in listaDeCaracteres)
-        {
-            Console.Write(itensListaDeCaracteres);
-        }
-
-        Console.WriteLine("\n\n=== Exercício 08 ===\n");
-
-        Console.WriteLine("Digite um número: ");
-        decimal numeroDigitado2 = decimal.Parse(Console.ReadLine());
-
-        decimal fatoracaoDeNumero = 1;
-
-        for (decimal numeroParaFatorar = numeroDigitado2; numeroParaFatorar >= 1; numeroParaFatorar--)
-            fatoracaoDeNumero *= numeroParaFatorar;
-
-        Console.WriteLine($"\nO resultado da fatoração do número {numeroDigitado2}! é: {fatoracaoDeNumero}");
-
-        Console.WriteLine("\n=== Exercício 09 ===\n");
-
-        int[] listaDeNumerosInteiros = [10, -3, 98, 7, 42];
-
-        int maiorValor = int.MinValue;
-        int menorValor = int.MaxValue;
-
-        Console.Write("Lista de números interios: ");
-        foreach (var itemDaLista in listaDeNumerosInteiros)
-        {
-            Console.Write($"{itemDaLista} ");
-
-            if (itemDaLista > maiorValor)
-                maiorValor = itemDaLista;
-
-            if (itemDaLista < menorValor)
-                menorValor = itemDaLista;
-        }
-
-        Console.WriteLine($"\nMaior número da lista: {maiorValor}");
-        Console.WriteLine($"Menor número da lista: {menorValor}");
-
-        Console.WriteLine("\n=== Exercício 10 ===\n");
-
-        int[,] matrizTransposta = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-
-        Console.WriteLine("Matriz original: ");
-
-        for (int linhaMatriz = 0; linhaMatriz < matrizTransposta.GetLength(0); linhaMatriz++)
-        {
-            for (int colunaMatriz = 0; colunaMatriz < matrizTransposta.GetLength(1); colunaMatriz++)
-            {
-                Console.Write($"{matrizTransposta[linhaMatriz, colunaMatriz]} ");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine("\nMatriz transposta: ");
-
-        for (int linhaMatriz = 0; linhaMatriz < matrizTransposta.GetLength(0); linhaMatriz++)
-        {
-            for (int colunaMatriz = 0; colunaMatriz < matrizTransposta.GetLength(1); colunaMatriz++)
-            {
-                Console.Write($"{matrizTransposta[colunaMatriz, linhaMatriz]} ");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine($"\nFinal GPA:\t\t{digitoPrincipal}.{primeiroDigito}{segundoDigito}");
     }
 }
